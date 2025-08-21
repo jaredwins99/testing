248e1a3c2a598cc78b0bab5fc33a68b6807e9ce5
jaredwins99 <jaredwins99@gmail.com>
2025-08-21 12:50:34 -0400

    added file

---
+++ orchestrate_one.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(dplyr)
+library(future)
+library(furrr)
+library(reticulate)
+library(mlflow)
+library(conflicted)
+
+#source(".Rprofile")
+source("run_analysis.R")
+#source(".Rprofile")
+source("run_analysis_nopred.R")
+
+c("select", "filter") %>% walk(~ conflict_prefer(.x, "dplyr"))
+c("year", "month") %>% walk(~ conflict_prefer(.x, "lubridate"))
+c("map") %>% walk( ~ conflict_prefer(.x, "purrr"))
+c("sd") %>%  walk(~ conflict_prefer(.x, "stats"))
+c("match") %>%  walk(~ conflict_prefer(.x, "base"))
+
+its_outcomes <- c("chicken_fish","meat","nonvegan","total","vegan","vegetarian")
+targeted_its_outcomes <- c("breakfast","untextured","textured")
+targeted_customer_outcomes <- c("breakfast","untextured")
+
+targeted_its_restaurants <- list(breakfast = c('2HRX9P6HKXA8V','JHDN7CF1C03X5','L69HYJ4Y3TR91','ED5J990H5VAZT'),
+                                 untextured = c('SRQS8F7JWA9MZ','JHDN7CF1C03X5'),
+                                 textured = c('VLZX7K2M9QD4T'))
+targeted_customer_restaurants <- list(breakfast = c('2HRX9P6HKXA8V','L69HYJ4Y3TR91','ED5J990H5VAZT'),
+                                      untextured = c('SRQS8F7JWA9MZ'))
+
+#run_its(outcome="nonvegan", directory="official_redux")
+run_nopred_its(outcome="nonvegan")
